fc301f8b8ffb0d239b900a94d4f28aa6ca87e1ff
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-05 17:23:07 -0300

    Changing the commit message on git setup recipe.

--- recipes/git_setup.rb
+++ recipes/git_setup.rb
@@ -1,4 +1,5 @@
 git :init
-git_add_all_and_commit_with_message "First commit! Just adding the basic Rails application produced by rails command."
+git_add_all_and_commit_with_message "Just adding the basic Rails application produced by rails command."
 git :remote => "add origin git@github.com:elomarns/#{app_name}.git"
+git :pull => "origin master"
 git :push => "origin master"
