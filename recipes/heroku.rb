0e7cfef8c5c92e89555ea4ab25bf958553185784
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-12 17:06:21 -0300

    Updating Heroku recipe to use Cedar stack.

--- recipes/heroku.rb
+++ recipes/heroku.rb
@@ -1,10 +1,10 @@
 run "gem install heroku --no-ri --no-rdoc"
 
-run "heroku create #{app_name.dasherize}"
+run "heroku create #{app_name.dasherize} --stack cedar"
 
 git :push => "heroku master"
 
-run "heroku rake db:migrate"
-run "heroku rake db:seed"
+run "heroku run rake db:migrate"
+run "heroku run rake db:seed"
 
 run "heroku open"
