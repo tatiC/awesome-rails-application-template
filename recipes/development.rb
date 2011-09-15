dcbbf132542ecaa09ce0b92fba940dfb5263b1da
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-15 10:52:44 -0300

    Improving commit message after changing development.rb file.

--- recipes/development.rb
+++ recipes/development.rb
@@ -1,3 +1,3 @@
 copy_template_file "development.rb", :to => "config/environments/development.rb"
 gsub_file "config/environments/development.rb", "ApplicationName", app_name.camelize
-git :commit => "-am 'Cleaning development environment configuration file.'"
+git :commit => "-am 'Cleaning and improving development environment configuration file.'"
