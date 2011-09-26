9d5b892d016c5efccbbbb6bc228164667fb11779
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-26 16:10:57 -0300

    Adding support to heroku_plus.

--- recipes/heroku.rb
+++ recipes/heroku.rb
@@ -1,10 +1,10 @@
 run "gem install heroku --no-ri --no-rdoc"
-
 run "heroku create #{app_name.dasherize} --stack cedar"
 
+git :remote => "rm heroku"
+git :remote => "add heroku git@official.heroku.com:#{app_name.dasherize}.git"
 git :push => "heroku master"
 
 run "heroku run rake db:migrate"
 run "heroku run rake db:seed"
-
-run "heroku open"
+run "heroku open --app #{app_name.dasherize}"
