b9b7aab9c92896445193ed6ba2708a7561834ae6
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-29 00:16:01 -0300

    Changing the first commit message on generated application.

--- recipes/git_setup.rb
+++ recipes/git_setup.rb
@@ -1,2 +1,2 @@
 git :init
-git_add_all_and_commit_with_message "First commit! Just adding the basic Rails application produced by rails command."
+git_add_all_and_commit_with_message "First commit! Just adding the basic Rails application produced by rails new command."
