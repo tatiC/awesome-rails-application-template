de9737a5e3f80d080e213a3415b82b6ad0d371c6
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-09-29 16:21:25 -0300

    Adding defult url options to development environment.

--- templates/development.rb
+++ templates/development.rb
@@ -3,8 +3,10 @@ ApplicationName::Application.configure do
   config.action_controller.perform_caching = true
   
   config.whiny_nils = true
-  config.action_mailer.raise_delivery_errors = true
   config.active_support.deprecation = :log
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
   
   config.consider_all_requests_local = true
   config.action_dispatch.best_standards_support = :builtin
