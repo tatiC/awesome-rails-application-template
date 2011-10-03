45b45881bc1cfdb454740b36f489a0e8e5368bca
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-03 18:09:35 -0300

    Adding the SendGrid addon properly.

--- recipes/heroku.rb
+++ recipes/heroku.rb
@@ -5,7 +5,7 @@ git :remote => "rm heroku"
 git :remote => "add heroku git@official.heroku.com:#{app_name.dasherize}.git"
 git :push => "heroku master"
 
-run "heroku addons:add sendgrid:starter"
+run "heroku addons:add sendgrid:starter --app #{app_name.dasherize}"
 
 run "heroku run rake db:migrate --app #{app_name.dasherize}"
 run "heroku run rake db:seed --app #{app_name.dasherize}"
