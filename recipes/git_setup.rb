6f81e5244920197168e8478ab6846a8be2b0264d
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-04 10:58:37 -0300

    Improving the .gitignore file of generated apps.

--- recipes/git_setup.rb
+++ recipes/git_setup.rb
@@ -1,2 +1,4 @@
+copy_template_file ".gitignore", :to => ".gitignore"
+
 git :init
 git_add_all_and_commit_with_message "First commit! Just adding the basic Rails application produced by rails new command."
