94dd8965bacf2ac300b560d1e713ac22dbd691b2
Elomar Nascimento dos Santos <elomarns@gmail.com>
2011-10-12 15:20:34 -0300

    Fixing admin assets recipe.

--- recipes/admin_assets.rb
+++ recipes/admin_assets.rb
@@ -1,9 +1,9 @@
 empty_directory "app/assets/images/admin"
 
 empty_directory "app/assets/stylesheets/admin"
-git :mv => "app/assets/stylesheets/application.css app/assets/stylesheets/admin/application.css"
+git :mv => "app/assets/stylesheets/application.css app/assets/stylesheets/admin/admin.css"
 
 empty_directory "app/assets/javascripts/admin"
-git :mv => "app/assets/javascripts/application.js app/assets/javascripts/admin/application.js"
+git :mv => "app/assets/javascripts/application.js app/assets/javascripts/admin/admin.js"
 
 git_add_all_and_commit_with_message "Renaming application.css and application.js to admin.css and admin.js."
